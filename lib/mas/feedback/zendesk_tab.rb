969f3d5e6f51fd0d18ff557a3db8545ffef8babf
Jared Fraser <dev@jsf.io>
2013-10-22 16:05:47 +0100

    Feedback tab no longer uses an image

--- lib/mas/feedback/zendesk_tab.rb
+++ lib/mas/feedback/zendesk_tab.rb
@@ -9,19 +9,13 @@ module MAS
 
       def to_tag(tag_type=:div)
         helper.content_tag tag_type, id: 'feedback-tab' do
-          feedback_link do
-            image_tag 
-          end
+          feedback_link
         end
       end
 
       def feedback_link(&block)
-        helper.link_to url, 
+        helper.link_to config[:name], url, 
             onClick: "script: Zenbox.show(); return false;", &block
-      end
-
-      def image_tag
-        helper.image_tag "mas/feedback/feedback-#{locale}.png", alt: helper.t('feedback.zendesk.open')
       end
 
       def url
