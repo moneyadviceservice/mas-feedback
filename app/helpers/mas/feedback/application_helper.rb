e479e60924492ffe61155e6e0fe255a8c3b7b152
Jared Fraser <dev@jsf.io>
2013-11-26 14:19:00 +0000

    Ignore missing configuration in tab helper

--- app/helpers/mas/feedback/application_helper.rb
+++ app/helpers/mas/feedback/application_helper.rb
@@ -3,7 +3,9 @@ module Mas
   module Feedback
     module ApplicationHelper
       def zendesk_feedback_tab(config, header=:head)
-        render('mas/feedback/zendesk', 
+        return if config.blank?
+
+        render('mas/feedback/zendesk',
           config: config,
           header: header) +
           MAS::Feedback::ZendeskTab.new(self, config, I18n.locale).to_tag
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,18 +4,33 @@ module Zendesk
   module Feedback
 
     describe ApplicationHelper do
-      let(:config)      { double(:[] => { id: 1, url: 2 }) }
+
 
       describe '#zenbox_feedback_tab' do
-        
         subject(:output)  { helper.zendesk_feedback_tab(config) }
-        
-        it 'includes the dropbox id' do
-          expect(output).to match(/dropboxID\:\s*"1"/)
+
+        context 'config present' do
+          let(:config) { double(:[] => { id: 1, url: 2 }) }
+
+          it 'includes the dropbox id' do
+            expect(output).to match(/dropboxID\:\s*"1"/)
+          end
+
+          it 'includes the dropbox url' do
+            expect(output).to match(/url\:\s*"2"/)
+          end
         end
 
-        it 'includes the dropbox url' do
-          expect(output).to match(/url\:\s*"2"/)
+        describe 'config missing' do
+          ["", nil, [], {}].each do |missing|
+            context "blank with #{missing.inspect}" do
+              let(:config)  { missing }
+
+              it 'returns blank' do
+                expect(output).to be_blank
+              end
+            end
+          end
         end
       end
     end
